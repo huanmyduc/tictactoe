5b4c70b0eaef7aec08b97789b22558875e56a2dc
huuhuan <huanlt2001@gmail.com>
2023-08-21 13:50:00 +0700

    sua lan 3

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
- const cells = document.querySelectorAll('.cell')
- const statusText = document.querySelector('.statusText')
- const restartBtn = document.querySelector('.restartBtn')
+const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector('.statusText');
+const restartBtn = document.querySelector('.restartBtn');
+let currentPlayer = "X"
+let gameActive = true;
 
 
-let xTurn = true;
-let count = 0;
-isGameOver = false;
-cells.forEach((e)=>{
-    e.addEventListener('click',()=>{
-        if(!e.textContent){
-            if(xTurn){
-                e.innerText = "X";
-            }else{
-                e.innerText = "0";
-            }
-        }
-        xTurn =!xTurn;
-        e.disabled = true;
-    })
-    
-})
-let conditionsWin = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-function checkWin(){
-    for(let i = 0; i < conditionsWin.length; i++){
-        let cell1 = cells[conditionsWin[i][0]].innerHTML;
-        let cell2 = cells[conditionsWin[i][1]].innerHTML
-        let cell3 = cells[conditionsWin[i][2]].innerHTML
-        
-        if(cell1 != "" && cell1 === cell2 && cell1 == cell3){
-            isGameOver = true;
-        
-        }
-    } 
+function cellClick(e){
+    const cell = e.target
+    if(!gameActive || cell.textContent !== ""){
+        return;
+    }
+    cell.textContent = currentPlayer;
+    checkWinner();
+    if(gameActive){
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        statusText.textContent = `Đến lượt người chơi ${currentPlayer}`;
+    }
 }
-checkWin()
+function checkWinner(){
+    let conditionsWin = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]          
+    ];
+    for(let i = 0; i < conditionsWin.length; i++){
+        const [a, b, c] = conditionsWin[i]
+        if(cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent){
+            gameActive = false;
+            statusText.textContent = `Người chơi ${currentPlayer} thắng!`
+        }
+    }
+}
+function restartClick(){
+    for(let i = 0 ; i < cells.length; i++){
+        cells[i].textContent = ""
+        statusText.textContent = ""
+        currentPlayer = "X"
+        gameActive = true;
+
+    }
+}
+
+
+for(let i = 0; i< cells.length; i ++){
+    const cell = cells[i]
+    cell.addEventListener("click",cellClick)
+}
+restartBtn.addEventListener("click",restartClick)
